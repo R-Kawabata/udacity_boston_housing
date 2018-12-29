d5accaca9fb967f266aec0696ff1b4e27e790b59
Raphael Kawabata <raphael.kawabata@gmail.com>
2018-12-29 15:30:50 -0200

    Update visuals.py

--- visuals.py
+++ visuals.py
@@ -11,16 +11,27 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 import matplotlib.pyplot as pl
 import numpy as np
-import sklearn.learning_curve as curves
+
+# ==== sklearn update 0.18 to 0.20 ==== #
+#import sklearn.learning_curve as curves
+from sklearn.model_selection import learning_curve as curves
+from sklearn.model_selection import validation_curve as v_curve
+
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.cross_validation import ShuffleSplit, train_test_split
+
+# ==== sklearn update 0.18 to 0.20 ==== #
+#from sklearn.cross_validation import ShuffleSplit, train_test_split
+from sklearn.model_selection import ShuffleSplit, train_test_split
 
 def ModelLearning(X, y):
     """ Calculates the performance of several models with varying sizes of training data.
         The learning and testing scores for each model are then plotted. """
     
     # Create 10 cross-validation sets for training and testing
-    cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
+    
+    # ==== sklearn update 0.18 to 0.20 ==== #
+    #cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
+    cv = ShuffleSplit(n_splits = 10, test_size = 0.2, train_size = 0.8, random_state = 0)
 
     # Generate the training set sizes increasing by 50
     train_sizes = np.rint(np.linspace(1, X.shape[0]*0.8 - 1, 9)).astype(int)
@@ -35,7 +46,9 @@ def ModelLearning(X, y):
         regressor = DecisionTreeRegressor(max_depth = depth)
 
         # Calculate the training and testing scores
-        sizes, train_scores, test_scores = curves.learning_curve(regressor, X, y, \
+        # ==== sklearn update 0.18 to 0.20 ==== #
+        #sizes, train_scores, test_scores = curves.learning_curve(regressor, X, y, \
+        sizes, train_scores, test_scores = curves(regressor, X, y, \
             cv = cv, train_sizes = train_sizes, scoring = 'r2')
         
         # Find the mean and standard deviation for smoothing
@@ -72,14 +85,17 @@ def ModelComplexity(X, y):
         The learning and testing errors rates are then plotted. """
     
     # Create 10 cross-validation sets for training and testing
-    cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
+    # ==== sklearn update 0.18 to 0.20 ==== #
+    #cv = ShuffleSplit(X.shape[0], n_iter = 10, test_size = 0.2, random_state = 0)
+    cv = ShuffleSplit(n_splits = 10, test_size = 0.2, train_size = 0.8, random_state = 0)
 
     # Vary the max_depth parameter from 1 to 10
     max_depth = np.arange(1,11)
 
     # Calculate the training and testing scores
-    train_scores, test_scores = curves.validation_curve(DecisionTreeRegressor(), X, y, \
-        param_name = "max_depth", param_range = max_depth, cv = cv, scoring = 'r2')
+    # ==== sklearn update 0.18 to 0.20 ==== #
+    #train_scores, test_scores = curves.validation_curve(DecisionTreeRegressor(), X, y, \
+    train_scores, test_scores = v_curve(DecisionTreeRegressor(), X, y, param_name = "max_depth", param_range = max_depth, cv = cv, scoring = 'r2')
 
     # Find the mean and standard deviation for smoothing
     train_mean = np.mean(train_scores, axis=1)
